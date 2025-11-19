29fcf542d07dbf9e8843c2ba8c2f0c2574c3d119
pasi12332 <hofmannpascal6@gmail.com>
2025-11-19 11:25:04 +0100

    ssomething

--- finale_abgabe/moviedb-jdbc-vorlage/src/main/java/de/hsh/dbs2/imdb/logic/MovieManager.java
+++ finale_abgabe/moviedb-jdbc-vorlage/src/main/java/de/hsh/dbs2/imdb/logic/MovieManager.java
@@ -2,7 +2,6 @@ package de.hsh.dbs2.imdb.logic;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
